f1f4cf3443f39f2df6742a1fe3402616d03a6c43
Jose Segura <jossegura2023@u.northwestern.edu>
2023-02-10 18:21:15 -0600

    first few touches to hw

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generateBtn(){
+function generatePassword(){
+// console.log("hey! You clicked me") 
+// 1. prompts for the user to apply the password criteria
+var lowerCase = prompt("Should password inlcude any lowercase (abcdefghijklmnopqrstuvwxyz):", "enter response"); 
+var upperCase = prompt("Should password inlcude any uppercase (ABCDEFGHIJKLMNOPQRSTUVWXYZ):", "enter response"); 
+var number = prompt("Should password inlcude any numbers (0123456789):", "enter response"); 
+var specialChar = prompt("Should password inlcude any special characters (!@#$%^&*()-_=+<>?):", "enter response"); 
+//  a. password length 8 < 128
+var charLength = 128
+    charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+<>?'
+//  b. lowercase, uppercase, numbers and special characters
+// 2. validate the input.
+// 3. password generates when at least one criteria is met
 
-  // 
-  
-  
-  
-  
-  //
 
+// 4. display password in an alert box or written on page (returning password)
+
+  return "Generated Password will go here";
+  
 
 }
 
